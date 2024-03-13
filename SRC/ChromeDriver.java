7900fc1abd9ce5024ce550e8bf2cc9937398e93c
Adam R <arosa@test.com>
2024-03-14 00:44:10 +0100

    Sample commit message8

---
+++ SRC/ChromeDriver.java
@@ -0,0 +1,12 @@
+public class ChromeDriver implements WebDriver {
+    @Override
+    public void get() {
+        System.out.println("Otwórz przegladarke Chrome driver");
+    }
+
+    @Override
+    public void findElementBy() {
+        System.out.println("Znajdz element na stronie chrome");
+
+    }
+}
---
+++ SRC/FirefoxDriver.java
@@ -0,0 +1,12 @@
+public class FirefoxDriver implements WebDriver {
+    @Override
+    public void get() {
+        System.out.println("Otworz przegladarke firefox");
+    }
+
+    @Override
+    public void findElementBy() {
+        System.out.println("Znajdz element na stronie firefox");
+
+    }
+}
